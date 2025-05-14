795539a6c243dd1004f630d2bfaffc4156ce0379
SKA1LERZWER <qroma46@gmail.com>
2025-05-14 12:37:01 +0300

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">
-    <?php
-    $host = 'MySQL-8.0';
-    $user = 'root';
-    $pass = '';
-    $name = 'users';
-    $link = mysqli_connect($host, $user, $pass, $name);
-    mysqli_query($link, "SET NAMES 'utf8'");
-?>
